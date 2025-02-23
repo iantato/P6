25a5ee021b3bcbb83ff89b66175e4024f0d26c5a
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-02-24 02:04:05 +0800

    Fixed laser

--- scenes/blocks/laser/laser.gd
+++ scenes/blocks/laser/laser.gd
@@ -15,6 +15,7 @@ var laser_atlas = {
 var tilemap: TileMapLayer
 var lasermap: TileMapLayer
 var start_tile: Vector2i
+var atlas_id = 8
 
 @onready var killzone = $KillZone 
 
@@ -23,7 +24,7 @@ func _ready():
 	tilemap = get_parent().get_parent().get_node("Ground")
 	lasermap = get_parent().get_parent().get_node("Laser")
 	start_tile = tilemap.local_to_map(tilemap.to_local(global_position)) + direction
-	lasermap.set_cell(start_tile, 7, laser_atlas.get(color))
+	lasermap.set_cell(start_tile, atlas_id, laser_atlas.get(color))
 	var cells_to_change = detect_collision(start_tile)
 	initialize_killzone(start_tile, cells_to_change)
 	shoot(cells_to_change)
@@ -45,8 +46,8 @@ func detect_collision(start_pos):
 func shoot(blocks_to_change: Array):
 	for tile in blocks_to_change:
 		print(tile)
-		lasermap.set_cell(tile, 7, laser_atlas.get(color))  # Add laser tile
-	lasermap.set_cell(blocks_to_change[-1] + Vector2i(0, 1), 7, laser_atlas.get(color) + Vector2(0, 1))
+		lasermap.set_cell(tile, atlas_id, laser_atlas.get(color))  # Add laser tile
+	lasermap.set_cell(blocks_to_change[-1] + Vector2i(0, 1), atlas_id, laser_atlas.get(color) + Vector2(0, 1))
 	
 func initialize_killzone(start_tile: Vector2i, cells: Array):
 	killzone.global_position = tilemap.map_to_local(start_tile) + Vector2(0, (cells.size() / 2) * 16)

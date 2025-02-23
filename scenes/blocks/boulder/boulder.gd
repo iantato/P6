d55003e98e1880fba987a0d2d5b48ccd4b1b1643
Andrei Adolfo <adolfojanandreim@gmail.com>
2025-02-23 16:20:38 +0800

    Started adding friction for boulder

---
+++ scenes/blocks/boulder/boulder.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+var default_friction = 0.1
+var current_friction = default_friction
+var tilemap: TileMapLayer
+
+func _ready():
+	tilemap = get_parent().get_node("Friction")
+
+func _physics_process(delta):
+	current_friction = get_tile_friction()
+
+    # Apply friction to gradually slow down velocity
+	if linear_velocity.length() > 0.1:
+		var friction_force = -linear_velocity.normalized() * current_friction * delta * 100
+		apply_central_impulse(friction_force)
+
+func get_tile_friction() -> float:
+	if not tilemap:
+		return default_friction
+
+	var local_pos = tilemap.to_local(global_position)
+	var tile_pos = tilemap.local_to_map(local_pos)
+	var tile_data = tilemap.get_cell_tile_data(tile_pos)
+
+	if tile_data:
+		var friction = tile_data.get_custom_data("friction")
+		return friction if friction else default_friction
+
+	return default_friction

1089f1e6f8373b3548e6b69c20dec28e727ceda9
iantato <83338977+iantato@users.noreply.github.com>
2025-02-24 03:36:20 +0800

    Finished sql

--- scripts/sql.gd
+++ scripts/sql.gd
@@ -13,18 +13,27 @@ func _ready() -> void:
 	db.query("CREATE TABLE IF NOT EXISTS players (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, level INTEGER, time FLOAT)")
 	insert_player("Test")
 	get_player_id("Test")
+	insert_time(1, 2.3848938)
+	get_time(1)
 
 
 func insert_player(name: String) -> void:
 	var query = """INSERT INTO players (name) VALUES ('%s');""" % [name]
 	db.query(query)
-	
 
-func get_player_id(name: String) -> void:
-	var query = "SELECT * from players WHERE id = %d ORDER BY id DESC LIMIT 1"
+
+func insert_time(id: int, time: float) -> void:
+	var query = """UPDATE players SET time = %f WHERE id = %d""" % [time, id]
 	db.query(query)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func get_player_id(name: String) -> void:
+	var query = "SELECT * from players WHERE name = '%s' ORDER BY id DESC LIMIT 1" % name
+	db.query(query)
+	Globals.player_id = db.get_query_result()[0]['id']
+
+
+func get_time(id: int) -> float:
+	var query = "SELECT time FROM players WHERE id = %d" % [id]
+	db.query(query)
+	return db.get_query_result()

6d08565feffba9d0df8940a8145ec48deabd3163
iantato <83338977+iantato@users.noreply.github.com>
2025-02-24 02:32:51 +0800

    Added Timer

---
+++ scenes/ui/timer/scripts/timer.gd
@@ -0,0 +1,14 @@
+extends Label
+
+var elapsed_time: float = 0.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	elapsed_time += delta
+	update_display()
+
+func update_display():
+	var minutes = int(elapsed_time) / 60
+	var seconds = int(elapsed_time) % 60
+	
+	self.text = "%02d:%02d" % [minutes, seconds]
